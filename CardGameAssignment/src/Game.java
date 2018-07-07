00557afc18120d04ffa95b87d98e487e9ce562b0
Eric <devilking9820@hotmail.com>
2018-07-08 01:31:19 +0800

    Add few Functions

--- CardGameAssignment/src/Game.java
+++ CardGameAssignment/src/Game.java
@@ -1,2 +1,9 @@
 public class Game {
+    public static void main(String[] args){
+        DeckOfCards d1 = new DeckOfCards();
+        Player p1 = new Player(d1);
+
+        p1.showHand();
+
+    }
 }
--- CardGameAssignment/src/Player.java
+++ CardGameAssignment/src/Player.java
@@ -7,9 +7,11 @@ public class Player extends DeckOfCards {
     private ArrayList<Card> setCard;
 
 
-    public Player() {
+    public Player(DeckOfCards d1) {
         handCard = new ArrayList<Card>();
-        handCard.add(super.pullRandom());
+        for(int i=0;i<10;i++){
+            addCards(d1);
+        }
         setCard = new ArrayList<Card>();
     }
 
@@ -18,7 +20,7 @@ public class Player extends DeckOfCards {
             System.out.println("There are no cards hold by the player.");
         } else {
             for (int i = 0; i < 10; i++) {
-                handCard.get(i);
+                System.out.println(handCard.get(i).getRanks()+" "+handCard.get(i).getSuits());
             }
         }
     }
@@ -32,8 +34,8 @@ public class Player extends DeckOfCards {
         }
     }
 
-    public void addCards() {
-        handCard.add(super.pullRandom());
+    public void addCards(DeckOfCards d1) {
+        handCard.add(d1.pullRandom());
     }
 
     public Card findCard(int i){
