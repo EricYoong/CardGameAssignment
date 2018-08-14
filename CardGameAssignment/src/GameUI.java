9638fc039aadc5938630b7ff89756c03444d057e
Eric <devilking9820@hotmail.com>
2018-08-14 23:18:01 +0800

    Improving the ui function

--- CardGameAssignment/src/GameUI.java
+++ CardGameAssignment/src/GameUI.java
@@ -1,6 +1,6 @@
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -8,17 +8,28 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 
 public class GameUI extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
         /*Various boolean values needed throughout the game. Objects are needed
          instead of regular booleans because regular local booleans could not be referenced
          from inside the lambda expressions used for the event listeners and game loop
@@ -26,12 +37,13 @@ public class GameUI extends Application {
         boolean gameStarted = false;
         boolean gameOver = false;
 
+
         //TextField to insert player name
         TextField nameText = new TextField();
 
         //Object Player and the deck of card
         DeckOfCards deck = new DeckOfCards();
-        Player player;
+        ArrayList<Player> player = new ArrayList<Player>();
 
         /*Initialize the game loop*/
         Timeline gameLoop = new Timeline();
@@ -39,16 +51,22 @@ public class GameUI extends Application {
         /*Alerts that pop up at various points in the game*/
         Alert existNameWarning = new Alert(Alert.AlertType.WARNING, "That is an existing name!");
         existNameWarning.setHeaderText(null);
+        Alert emptyNameWarning = new Alert(Alert.AlertType.WARNING, "The name cannot be NULL!");
+        emptyNameWarning.setHeaderText(null);
         Alert wrongSetWarning = new Alert(Alert.AlertType.WARNING, "That is an incorrect set!");
         wrongSetWarning.setHeaderText(null);
 
 
         /*Declaring all buttons for the application*/
-        Button player2Button = new Button("2 Player");
-        Button player3Button = new Button("3 PLayer");
-        Button player4Button = new Button("4 PLayer");
-        Button instructionsButton = new Button("View Instructions");
-        Button exitButton = new Button("Exit Game");
+        MenuItem itemExit = new MenuItem("EXIT");
+        MenuItem item2Player = new MenuItem("2 Player");
+        MenuItem item3Player = new MenuItem("3 Player");
+        MenuItem item4Player = new MenuItem("4 Player");
+        MenuItem itemInstruction = new MenuItem("View Instructions");
+
+        MenuBox menuBox = new MenuBox(item2Player, item3Player, item4Player, itemInstruction, itemExit);
+        menuBox.setTranslateX(280);
+        menuBox.setTranslateY(300);
 
         Button backToMenuButton = new Button("Back to Menu");
         Button toMenuFromScores = new Button("Back To Menu");
@@ -80,18 +98,11 @@ public class GameUI extends Application {
         /*Panes containing buttons and labels that sit on top of Canvases in the Groups
           This is done in order to have buttons and other JavaFX controls mixed in with Canvas graphics
           being drawn underneath*/
-        Pane pane = new Pane();
+        Pane gamePane = new Pane();
         Pane menuPane = new Pane();
         Pane instructionsPane = new Pane();
 
-
-        menuPane.getChildren().add(player2Button);
-        menuPane.getChildren().add(player3Button);
-        menuPane.getChildren().add(player4Button);
-        menuPane.getChildren().add(instructionsButton);
-        menuPane.getChildren().add(exitButton);
-
-
+        menuPane.getChildren().add(menuBox);
 
 
         /*Declaring the scenes that will be used and adding the Groups to them
@@ -116,7 +127,7 @@ public class GameUI extends Application {
          /*Add the Canvases and Panes to the groups. Canvases must be added first
       for each Group or else the Canvas will cover the Pane*/
         root.getChildren().add(canvas);
-        root.getChildren().add(pane);
+        root.getChildren().add(gamePane);
         menu.getChildren().add(menuCanvas);
         menu.getChildren().add(menuPane);
         instructionsGroup.getChildren().add(instructionsCanvas);
@@ -136,44 +147,183 @@ public class GameUI extends Application {
 
 
         /*===BUTTON EVENT LISTENERS ===*/
-        player2Button.setOnAction(e -> {
-            primaryStage.setScene(gameScene);
-            primaryStage.setHeight(625);
-            primaryStage.setWidth(804);
+        itemExit.setOnMouseClicked(e -> System.exit(0));
 
+        item2Player.setOnMouseClicked(e -> {
+            String tmpName;
+            for (int i = 0; i < 2; i++) {
+                boolean nameExis = false;
+                while (nameExis == false) {
+                    TextInputDialog dialog = new TextInputDialog("walter");
+                    dialog.setTitle("Text Input Dialog");
+                    dialog.setHeaderText("Look, a Text Input Dialog");
+                    dialog.setContentText("Please enter your name:");
+                    Optional<String> result = dialog.showAndWait();
+
+                    if (result.isPresent()) {
+                        if (result.get() == null || result.get() == "" || result.get() == " ") {
+                            emptyNameWarning.showAndWait();
+                        } else {
+                            if (player.size() == 0) {
+                                player.add(new Player(result.get(), deck));
+                                nameExis = true;
+                            } else {
+                                for (int j = 0; j < player.size(); j++) {
+                                    if (player.get(j).checkName(result.get())) {
+                                        nameExis = false;
+                                        existNameWarning.showAndWait();
+                                        break;
+                                    } else {
+                                        nameExis = true;
+                                    }
+                                }
+                                if(nameExis == true)
+                                    player.add(new Player(result.get(), deck));
+                            }
+                        }
+                    }
+                }
+            }
         });
 
-        player3Button.setOnAction(e -> {
-            primaryStage.setScene(gameScene);
-            primaryStage.setHeight(625);
-            primaryStage.setWidth(804);
-            resetGame(gc, previousX, player, dealer, gameOver, dealersTurn, betFinished);
+        item3Player.setOnMouseClicked(e -> {
+            for (int i = 0; i < 3; i++) {
+                boolean nameExis = false;
+                while (nameExis == false) {
+                    TextInputDialog dialog = new TextInputDialog("walter");
+                    dialog.setTitle("Text Input Dialog");
+                    dialog.setHeaderText("Look, a Text Input Dialog");
+                    dialog.setContentText("Please enter your name:");
+                    Optional<String> result = dialog.showAndWait();
+
+                    if (result.isPresent()) {
+                        if (result.get() == null || result.get() == "" || result.get() == " ") {
+                            emptyNameWarning.showAndWait();
+                        } else {
+                            if (player.size() == 0) {
+                                player.add(new Player(result.get(), deck));
+                                nameExis = true;
+                            } else {
+                                for (int j = 0; j < player.size(); j++) {
+                                    if (player.get(j).checkName(result.get())) {
+                                        nameExis = false;
+                                        existNameWarning.showAndWait();
+                                        break;
+                                    } else {
+                                        nameExis = true;
+                                    }
+                                }
+                                if(nameExis == true)
+                                    player.add(new Player(result.get(), deck));
+                            }
+                        }
+                    }
+                }
+            }
         });
 
-        player4Button.setOnAction(e -> {
-            primaryStage.setScene(gameScene);
-            primaryStage.setHeight(625);
-            primaryStage.setWidth(804);
-            resetGame(gc, previousX, player, dealer, gameOver, dealersTurn, betFinished);
+        item4Player.setOnMouseClicked(e -> {
+
+            String tmpName;
+            Player tmpPlayer;
+            for (int i = 0; i < 4; i++) {
+                boolean nameExis = false;
+                while (nameExis == false) {
+                    TextInputDialog dialog = new TextInputDialog("walter");
+                    dialog.setTitle("Text Input Dialog");
+                    dialog.setHeaderText("Look, a Text Input Dialog");
+                    dialog.setContentText("Please enter your name:");
+                    Optional<String> result = dialog.showAndWait();
+
+                    if (result.isPresent()) {
+                        if (result.get() == null || result.get() == "" || result.get() == " ") {
+                            emptyNameWarning.showAndWait();
+                        } else {
+                            if (player.size() == 0) {
+                                player.add(new Player(result.get(), deck));
+                                nameExis = true;
+                            } else {
+                                for (int j = 0; j < player.size(); j++) {
+                                    if (player.get(j).checkName(result.get())) {
+                                        nameExis = false;
+                                        existNameWarning.showAndWait();
+                                        break;
+                                    } else {
+                                        nameExis = true;
+                                    }
+                                }
+                                if(nameExis == true)
+                                    player.add(new Player(result.get(), deck));
+                            }
+                        }
+                    }
+                    }
+                }
+            }
         });
 
-        instructionsButton.setOnAction(e -> primaryStage.setScene(instructionsScene));
 
-        exitButton.setOnAction(e -> {
-            Platform.exit();
-        });
-
-        backToMenuButton.setOnAction(e -> primaryStage.setScene(menuScene));
-
-        exitToMenuFromGameButton.setOnAction(e -> {
-            primaryStage.setScene(menuScene);
-        });
-
-
-        gameLoop.getKeyFrames().add(frame);
         gameLoop.play();
         primaryStage.show();
 
+    }
+
+    //Menu
+    private static class MenuBox extends VBox {
+        public MenuBox(MenuItem... items) {
+            getChildren().add(createSeparator());
+
+            for (MenuItem item : items) {
+                getChildren().addAll(item, createSeparator());
+            }
+        }
+
+        private Line createSeparator() {
+            Line sep = new Line();
+            sep.setEndX(200);
+            sep.setStroke(Color.DARKGREY);
+            return sep;
+        }
+    }
+
+    private static class MenuItem extends StackPane {
+        public MenuItem(String name) {
+            LinearGradient gradient = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[]{
+                    new Stop(0, Color.DARKVIOLET),
+                    new Stop(0.1, Color.BLACK),
+                    new Stop(0.9, Color.BLACK),
+                    new Stop(1, Color.DARKVIOLET)
+            });
+
+            Rectangle bg = new Rectangle(200, 30);
+            bg.setOpacity(0.4);
+
+            Text text = new Text(name);
+            text.setFill(Color.DARKGREY);
+            text.setFont(Font.font("Tw Cen MT Condensed", FontWeight.SEMI_BOLD, 22));
+
+            setAlignment(Pos.CENTER);
+            getChildren().addAll(bg, text);
+
+            setOnMouseEntered(event -> {
+                bg.setFill(gradient);
+                text.setFill(Color.WHITE);
+            });
+
+
+            setOnMouseExited(event -> {
+                bg.setFill(Color.BLACK);
+                text.setFill(Color.DARKGREY);
+            });
+
+            setOnMousePressed(event -> {
+                bg.setFill(Color.DARKVIOLET);
+            });
+
+            setOnMouseReleased(event -> {
+                bg.setFill(gradient);
+            });
+        }
     }
 
     public static void main(String[] args) {
